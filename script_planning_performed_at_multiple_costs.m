a4b1ac157b17420527eb32e2ad87732184a33a27
sjharnett <sjh6473@psu.edu>
2022-06-08 12:42:29 -0400

    adjusting plotting range for more r_D

--- script_planning_performed_at_multiple_costs.m
+++ script_planning_performed_at_multiple_costs.m
@@ -8,7 +8,7 @@ addpath([pwd '\PathPlanning_MapTools_MapGenClassLibrary\Functions'])
 addpath([pwd '\PathPlanning_GeomTools_GeomClassLibrary\Functions'])
 
 %% initialize loop params and storage arrays for plotting
-des_gap_size = linspace(0.001,0.038,10);%linspace(0.001,0.081,10);
+des_gap_size = linspace(0.0001,0.08,20);
 colors = [1 0 0; 0 1 0; 0 0 1; 0 1 1;1 0 1;1 1 0; 0 0 0;0 0.4470 0.7410; 0.8500 0.3250 0.0980; 0.9290 0.6940 0.1250; 0.4940 0.1840 0.5560; 0.4660 0.6740 0.1880; 0.3010 0.7450 0.9330; 0.6350 0.0780 0.1840; 1 0 0; 0 1 0; 0 0 1; 0 1 1;1 0 1;1 1 0];
 all_rd = [];
 fig = 99;
