f9210503c52ab8f3a8767a9f8cdc87add4a4d8fe
sjharnett <sjh6473@psu.edu>
2022-08-07 18:58:23 -0400

    plot many d_effs with middle_o

--- script_test_linear_occupancy.m
+++ script_test_linear_occupancy.m
@@ -2,13 +2,13 @@
 addpath([pwd '\Example_Map_Generation_Code'])
 addpath([pwd '\PathPlanning_MapTools_MapGenClassLibrary\Functions'])
 addpath([pwd '\PathPlanning_GeomTools_GeomClassLibrary\Functions'])
-% 
+%
 % %% initialize loop params and storage arrays for plotting
 % des_gap_size = linspace(0.0001,0.08,30);
 all_rd = [];
-% 
+%
 flag_do_plot = 0;
-% 
+%
 % %% initialize loop params and storage arrays for plotting
 % measured_unoccupancy = [];
 % est_from_sqrt_area_ratio_all = [];
@@ -26,9 +26,12 @@ est_d_eff = [];
 est_d_eff2 = [];
 est_d_eff3 = [];
 est_d_eff4 = [];
-% 
+est_d_eff5 = [];
+est_d_eff6 = [];
+est_d_eff7 = [];
+%
 % % generate Voronoi tiling from Halton points
-% 
+%
 % for halton_seeds = 0:2000:10000
 %     low_pt = 1+halton_seeds; high_pt = 1000+halton_seeds; % range of Halton points to use to generate the tiling
 %     trim_polytopes = fcn_MapGen_haltonVoronoiTiling([low_pt,high_pt],[1 1]);
@@ -36,7 +39,7 @@ est_d_eff4 = [];
 %         % shink the polytopes so that they are no longer tiled
 %         gap_size = des_gap_size(gap_idx); % desired average maximum radius
 %         shrunk_polytopes = fcn_MapGen_polytopesShrinkFromEdges(trim_polytopes,gap_size);
-% 
+%
 %         %% polytope stats to create inputs for predictor code
 %         field_stats = fcn_MapGen_polytopesStatistics(shrunk_polytopes);
 %         field_stats_pre_shrink = fcn_MapGen_polytopesStatistics(trim_polytopes);
@@ -46,7 +49,7 @@ est_d_eff4 = [];
 %         shrunk_distance = field_stats_pre_shrink.average_max_radius - field_stats.average_max_radius;
 %         shrink_ang = field_stats_pre_shrink.average_vertex_angle;
 %         R_bar_initial = field_stats_pre_shrink.average_max_radius;
-% 
+%
 %         %% find measured occupancy
 %         % set polytope traversal cost. it doesn't matter what this is, as long as it's known.
 %         des_cost = 0.2;
@@ -76,21 +79,24 @@ low_pt = 1; high_pt = 1000; % range of Halton points to use to generate the tili
 trim_polytopes = fcn_MapGen_haltonVoronoiTiling([low_pt,high_pt],[1 1]);
 
 %% begin loop of departure ratios
-for gap_idx = 1:1:18;%1:length(des_gap_size)
+for gap_idx = 1:1:14%1:length(des_gap_size)
     est_avg_circ_min_rad_est1_this_rd = [];
     est_d_eff_this_rd = [];
     est_d_eff_this_rd2 = [];
     est_d_eff_this_rd3 = [];
     est_d_eff_this_rd4 = [];
+    est_d_eff_this_rd6 = [];
+    est_d_eff_this_rd5 = [];
+    est_d_eff_this_rd7 = [];
     rd_this_rd = [];
-    for halton_seeds = 0:2000:10000
+    for halton_seeds = 0%:2000:10000
         low_pt = 1+halton_seeds; high_pt = 1000+halton_seeds; % range of Halton points to use to generate the tiling
         trim_polytopes = fcn_MapGen_haltonVoronoiTiling([low_pt,high_pt],[1 1]);
         % shink the polytopes so that they are no longer tiled
         gap_size = des_gap_size(gap_idx); % desired average maximum radius
         shrunk_polytopes = fcn_MapGen_polytopesShrinkFromEdges(trim_polytopes,gap_size);
 
-    
+
         %% polytope stats to create inputs for predictor code
         field_stats = fcn_MapGen_polytopesStatistics(shrunk_polytopes);
         field_stats_pre_shrink = fcn_MapGen_polytopesStatistics(trim_polytopes);
@@ -100,13 +106,13 @@ for gap_idx = 1:1:18;%1:length(des_gap_size)
         shrunk_distance = field_stats_pre_shrink.average_max_radius - field_stats.average_max_radius;
         shrink_ang = field_stats_pre_shrink.average_vertex_angle;
         R_bar_initial = field_stats_pre_shrink.average_max_radius;
-    
+
         %% get some basic parameters for plotting
         xlow = 0; xhigh = 1; ylow = 0; yhigh = 1;
         area = (xhigh-xlow)*(yhigh-ylow);
         rho = length(trim_polytopes)/area;
         rd_this_rd = [rd_this_rd, field_avg_r_D];
-    
+
         if flag_do_plot
             % plot the map
             line_spec = 'b-'; % edge line plotting
@@ -121,9 +127,12 @@ for gap_idx = 1:1:18;%1:length(des_gap_size)
         est_d_eff_this_rd2 = [est_d_eff_this_rd2, unocc_ests.L_unocc_est_d_eff2];
         est_d_eff_this_rd3 = [est_d_eff_this_rd3, unocc_ests.L_unocc_est_d_eff3];
         est_d_eff_this_rd4 = [est_d_eff_this_rd4, unocc_ests.L_unocc_est_d_eff4];
+        est_d_eff_this_rd5 = [est_d_eff_this_rd2, unocc_ests.L_unocc_est_d_eff5];
+%         est_d_eff_this_rd6 = [est_d_eff_this_rd3, unocc_ests.L_unocc_est_d_eff6];
+%         est_d_eff_this_rd7 = [est_d_eff_this_rd7, unocc_ests.L_unocc_est_d_eff7];
     end
     %% find estimated values
-    
+
 %     est_from_sqrt_area_ratio_all = [est_from_sqrt_area_ratio_all, (unocc_ests.A_unocc_meas).^0.5];
 %     est_from_gap_size_all = [est_from_gap_size_all, unocc_ests.L_unocc_est_gap_size];
 %     est_from_AABB_all = [est_from_AABB_all, unocc_ests.L_unocc_est_AABB_width];
@@ -137,6 +146,9 @@ for gap_idx = 1:1:18;%1:length(des_gap_size)
     est_d_eff2 = [est_d_eff2, mean(est_d_eff_this_rd2)];
     est_d_eff3 = [est_d_eff3, mean(est_d_eff_this_rd3)];
     est_d_eff4 = [est_d_eff4, mean(est_d_eff_this_rd4)];
+    est_d_eff5 = [est_d_eff5, mean(est_d_eff_this_rd5)];
+%     est_d_eff6 = [est_d_eff6, mean(est_d_eff_this_rd6)];
+%     est_d_eff7 = [est_d_eff7, mean(est_d_eff_this_rd7)];
     all_rd = [all_rd,mean(rd_this_rd)];
 end
 
@@ -157,6 +169,9 @@ plot(all_rd, est_d_eff)
 plot(all_rd, est_d_eff2)
 plot(all_rd, est_d_eff3)
 plot(all_rd, est_d_eff4)
+plot(all_rd, est_d_eff5)
+plot(all_rd, est_d_eff6)
+plot(all_rd, est_d_eff7)
 xlabel('departure ratio [r_D]');
 % measuring distance outside of polytopes for 1 km travel i.e. unoccupancy
 ylabel('linear unoccupancy ratio');
@@ -165,7 +180,10 @@ legend('measured from planner',...
     'estimate from d_{eff}',...
     'estimate from d_{eff}2',...
     'estimate from d_{eff}3',...
-    'estimate from d_{eff}4');
+    'estimate from d_{eff}4',...
+    'estimate from d_{eff}5',...
+    'estimate from d_{eff}6',...
+    'estimate from d_{eff}7');
 
 figure(1)
 box on
@@ -184,6 +202,9 @@ plot(all_rd, 1-est_d_eff)
 plot(all_rd, 1-est_d_eff2)
 plot(all_rd, 1-est_d_eff3)
 plot(all_rd, 1-est_d_eff4)
+plot(all_rd, 1-est_d_eff5)
+plot(all_rd, 1-est_d_eff6)
+plot(all_rd, 1-est_d_eff7)
 xlabel('departure ratio [r_D]');
 % measuring distance outside of polytopes for 1 km travel i.e. unoccupancy
 ylabel('linear occupancy ratio');
@@ -192,7 +213,10 @@ legend('measured from planner',...
     'estimate from d_{eff}',...
     'estimate from d_{eff}2',...
     'estimate from d_{eff}3',...
-    'estimate from d_{eff}4');
+    'estimate from d_{eff}4',...
+    'estimate from d_{eff}5',...
+    'estimate from d_{eff}6',...
+    'estimate from d_{eff}7');
 
 
 % figure(3)
