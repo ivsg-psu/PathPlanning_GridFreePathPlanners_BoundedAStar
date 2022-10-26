2f69e4ae64e040c3749f0aa3a7bc71e2d1ce4d67
sjharnett <sjh6473@psu.edu>
2022-10-26 16:51:45 -0400

    weighted_visibility_matrix started

---
+++ fcn_find_edge_weights.m
@@ -0,0 +1,9 @@
+function weighted_edge_map = fcn_find_edge_weights(all_pts)
+    % all_pts = [xv' yv' [1:length(xv)]' obs_id beg_end];
+    % pts : obs_id
+    % want to form a matrix of 1 and 0 for visibility
+    % this gives all possible edges
+    % edges weights : min(obs_id1 obs_id2)
+    % x coord array of all x's
+    % y coord array of all y's
+end
