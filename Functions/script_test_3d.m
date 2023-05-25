5155ee515faea72765cb63449ba0635860dc9f7c
Stephen J. Harnett <sjh6473@arl.psu.edu>
2023-05-25 10:57:33 -0400

    adding links

--- Functions/script_test_3d.m
+++ Functions/script_test_3d.m
@@ -1,6 +1,6 @@
 clear all; close all; clc
 
-polytopes(1).vertices = [1 1 0; 2 1 0;  3 1 20;2 1 20]; % a line that translates its length in x over the course of 20 seconds
+polytopes(1).vertices = [1 1 0; 2 1 0;  3 1 20; 2 1 20]; % a line that translates its length in x over the course of 20 seconds
 start = [2 0 0];
 finish = [2 2 15];
 fill3(polytopes(1).vertices(:,1),polytopes(1).vertices(:,2),polytopes(1).vertices(:,3),'b');
@@ -8,7 +8,9 @@ box on; hold on;
 plot3(start(1),start(2),start(3),'gx');
 plot3(finish(1),finish(2),finish(3),'rx');
 
+https://www.mathworks.com/matlabcentral/fileexchange/33073-triangle-ray-intersection
 https://en.wikipedia.org/wiki/Intersection_of_a_polyhedron_with_a_line
+https://www.mathworks.com/help/matlab/visualize/multifaceted-patches.html
 % at each time t, calculate P
 % ir for all points on all bodies that are candidates for collision
 % select one point P
