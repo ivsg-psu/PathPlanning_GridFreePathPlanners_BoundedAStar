2ee7de01e1bec3585d0e7564a38c25902fa7496f
sjharnett <82562987+sjharnett@users.noreply.github.com>
2023-06-02 15:30:51 -0400

    adding two facet vgraph and discarding shallow rays

--- Functions/script_test_3d_multiple_facets.m
+++ Functions/script_test_3d_multiple_facets.m
@@ -5,7 +5,6 @@ opts.height     = 6;
 opts.fontType   = 'Times';
 opts.fontSize   = 9;
 
-
 facets = [];
 
 % verts (Nx3 array of verts)
@@ -13,23 +12,12 @@ facets = [];
 % obsID (ID for polytope obstacle it belongs to)
 
 verts = [1 1 0; 2 1 0;  3 1 20; 2 1 20]; % a line that translates its length in x over the course of 20 seconds
+verts2 = [2 2 10; 3 2 10;  4 2 30; 3 2 30]; % a line that translates its length in x over the course of 20 seconds
 start = [2 0 0];
-finish = [2 2 30];
-figure; hold on; box on; title('surfels and line from start to goal')
+finish = [4 4 45];
+figure; hold on; box on; title('surfels and line from start to goal');
 fig = gcf;
-fill3(verts(1:3,1),verts(1:3,2),verts(1:3,3),'b');
-fill3(verts([1,3,4],1),verts([1,3,4],2),verts([1,3,4],3),'r');
-plot3(start(1),start(2),start(3),'gx');
-plot3(finish(1),finish(2),finish(3),'rx');
-plot3([start(1) finish(1)],[start(2) finish(2)],[start(3) finish(3)])
-% finish = start + dir
-% thus dir = finish-start
-[intersect, t, u, v, xcoor] = TriangleRayIntersection (start, finish-start, verts(1,:),verts(2,:),verts(3,:),'lineType','segment','border','exclusive')
-[intersect2, t2, u2, v2, xcoor2] = TriangleRayIntersection (start, finish-start, verts(1,:),verts(3,:),verts(4,:),'lineType','segment')
-plot3(xcoor(1),xcoor(2),xcoor(3),'cx')
-legend('tri 1','tri 2','start','goal','','intersection')
 % scaling
-fig.Units               = 'centimeters';
 fig.Position(3)         = opts.width;
 fig.Position(4)         = opts.height;
 
@@ -43,15 +31,33 @@ set(gca,'LooseInset',max(get(gca,'TightInset'), 0.02))
 xlabel('x [m]')
 ylabel('y [m]')
 zlabel('t [s]')
-
+fill3(verts(1:3,1),verts(1:3,2),verts(1:3,3),'b');
+fill3(verts([1,3,4],1),verts([1,3,4],2),verts([1,3,4],3),'r');
+fill3(verts2(1:3,1),verts2(1:3,2),verts2(1:3,3),'g');
+fill3(verts2([1,3,4],1),verts2([1,3,4],2),verts2([1,3,4],3),'y');
+plot3(start(1),start(2),start(3),'gx');
+plot3(finish(1),finish(2),finish(3),'rx');
+plot3([start(1) finish(1)],[start(2) finish(2)],[start(3) finish(3)])
+% finish = start + dir
+% thus dir = finish-start
+[intersect, t, u, v, xcoor] = TriangleRayIntersection (start, finish-start, verts(1,:),verts(2,:),verts(3,:),'lineType','segment','border','exclusive')
+[intersect2, t2, u2, v2, xcoor2] = TriangleRayIntersection (start, finish-start, verts(1,:),verts(3,:),verts(4,:),'lineType','segment')
+[intersect3, t3, u3, v3, xcoor3] = TriangleRayIntersection (start, finish-start, verts2(1,:),verts2(2,:),verts2(3,:),'lineType','segment','border','exclusive')
+[intersect4, t4, u4, v4, xcoor4] = TriangleRayIntersection (start, finish-start, verts2(1,:),verts2(3,:),verts2(4,:),'lineType','segment','border','exclusive')
+plot3(xcoor(1),xcoor(2),xcoor(3),'cx')
+plot3(xcoor3(1),xcoor3(2),xcoor3(3),'cx')
+plot3(xcoor4(1),xcoor4(2),xcoor4(3),'cx')
+legend('tri 1','tri 2','tri3','tri4','start','goal','','intersection')
 
 %% this code is required to vectorize the edge, triangle intersection checking
-all_pts = [verts; start; finish];
-all_surfels = [verts(1,:),verts(2,:),verts(3,:);verts(1,:),verts(3,:),verts(4,:)]
+all_pts = [verts; verts2; start; finish];
+all_surfels = [verts(1,:),verts(2,:),verts(3,:);verts(1,:),verts(3,:),verts(4,:);verts2(1,:),verts2(2,:),verts2(3,:);verts2(1,:),verts2(3,:),verts2(4,:)]
 figure; hold on; box on; title('all vertices and start and finish')
+fig = gcf;
 plot3(start(1),start(2),start(3),'gx');
 plot3(finish(1),finish(2),finish(3),'rx');
 plot3(verts(:,1),verts(:,2),verts(:,3),'cx')
+plot3(verts2(:,1),verts2(:,2),verts2(:,3),'cx')
 
 num_pts = size(all_pts,1); % number of rows
 all_pts_idx = 1:1:num_pts; % array of all possible pt idx
@@ -69,6 +75,7 @@ fig.Position(3)         = opts.width;
 fig.Position(4)         = opts.height;
 
 % set text properties
+fig.Units               = 'centimeters';
 set(fig.Children, ...
     'FontName',     'Times', ...
     'FontSize',     9);
@@ -81,8 +88,8 @@ zlabel('t [s]')
 for i = 1:1:num_rays
     plot3([all_ray_starts(i,1), all_ray_ends(i,1)],[all_ray_starts(i,2), all_ray_ends(i,2)],[all_ray_starts(i,3), all_ray_ends(i,3)],'LineWidth',2)
 end
+
 figure; hold on; box on; title('vgraph')
-fig = gcf;
 % scaling
 fig.Units               = 'centimeters';
 fig.Position(3)         = opts.width;
@@ -121,25 +128,8 @@ for k = 1:1:length(intersects_idx)
 end
 fill3(verts(1:3,1),verts(1:3,2),verts(1:3,3),'b','FaceAlpha',0.3);
 fill3(verts([1,3,4],1),verts([1,3,4],2),verts([1,3,4],3),'b','FaceAlpha',0.3);
-
-%% discard rays that are too high in velocity
-% ray slope is rise over run
-% rise is delta t
-all_delta_ts = all_ray_ends(:,3) - all_ray_starts(:,3);
-% run is change in total length regardless of x or y
-all_delta_xs = all_ray_ends(:,1) - all_ray_starts(:,1);
-all_delta_ys = all_ray_ends(:,2) - all_ray_starts(:,2);
-all_delta_dist = (all_delta_xs.^2 + all_delta_ys.^2).^0.5;
-all_slopes = all_delta_ts./all_delta_dist;
-
-speed_violation_idx = find(all_slopes == 0);
-for l = 1:1:length(speed_violation_idx)
-    i = speed_violation_idx(l);
-    plot3([all_ray_starts(i,1), all_ray_ends(i,1)],[all_ray_starts(i,2), all_ray_ends(i,2)],[all_ray_starts(i,3), all_ray_ends(i,3)],'k','LineWidth',2)
-end
-
-
-
+fill3(verts2(1:3,1),verts2(1:3,2),verts2(1:3,3),'b','FaceAlpha',0.3);
+fill3(verts2([1,3,4],1),verts2([1,3,4],2),verts2([1,3,4],3),'b','FaceAlpha',0.3);
 % https://www.mathworks.com/matlabcentral/fileexchange/33073-triangle-ray-intersection
 % https://en.wikipedia.org/wiki/Intersection_of_a_polyhedron_with_a_line
 % https://www.mathworks.com/help/matlab/visualize/multifaceted-patches.html
