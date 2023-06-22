1ae2f0ff59cec264099c380804a3b197d33487a7
sjharnett <sjh6473@psu.edu>
2023-06-22 13:16:56 -0400

    draft of reachability function

---
+++ Functions/fcn_check_reachability.m
@@ -0,0 +1,15 @@
+function [b_is_reachable, num_steps] = fcn_check_reachability(vgraph,start,finish)
+    num_pts = size(vgraph,1);
+    start_id = start(4);
+    finish_id = finish(:,4);
+    start_id_repeated = ones(size(finish_id,1),size(finish_id,2))*start_id;
+    for num_steps = 1:num_pts
+        rgraph = vgraph^num_steps;
+        ind = sub2ind([num_pts,num_pts],start_id_repeated,finish_id);
+        if sum(rgraph(ind)) > 0
+            b_is_reachable = 1;
+            return
+        end
+    end
+    b_is_reachable = 0;
+endk
