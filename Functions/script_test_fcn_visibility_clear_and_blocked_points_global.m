09e5c03521edc2087f46fd42c71151a362374ced
Stephen J. Harnett <sjh6473@arl.psu.edu>
2024-01-10 17:09:45 -0500

    WIP tests for concave obstacles added, started moving enclave ray check to 2D

--- Functions/script_test_fcn_visibility_clear_and_blocked_points_global.m
+++ Functions/script_test_fcn_visibility_clear_and_blocked_points_global.m
@@ -18,7 +18,183 @@ addpath([pwd '\..\PathPlanning_MapTools_MapGenClassLibrary\Functions'])
 addpath([pwd '\..\PathPlanning_GeomTools_GeomClassLibrary\Functions'])
 
 flag_do_plot = 1;
+%% convex polytope
+convex_polytope(1).vertices = [0 0; 1 1; -1 2; -2 1; -1 0; 0 0];
+polytopes = fcn_MapGen_fillPolytopeFieldsFromVertices(convex_polytope);
 
+% plot the map
+if flag_do_plot
+    fig = 111; % figure to plot on
+    line_spec = 'b-'; % edge line plotting
+    line_width = 2; % linewidth of the edge
+    axes_limits = [-3 3 -3 3]; % x and y axes limits
+    axis_style = 'square'; % plot axes style
+    fcn_plot_polytopes(polytopes,fig,line_spec,line_width,axes_limits,axis_style);
+    hold on
+    box on
+    xlabel('x [km]')
+    ylabel('y [km]')
+    title('valid edges')
+    fig = 112; % figure to plot on
+    line_spec = 'b-'; % edge line plotting
+    line_width = 2; % linewidth of the edge
+    axes_limits = [-3 3 -3 3]; % x and y axes limits
+    axis_style = 'square'; % plot axes style
+    fcn_plot_polytopes(polytopes,fig,line_spec,line_width,axes_limits,axis_style);
+    hold on
+    box on
+    xlabel('x [km]')
+    ylabel('y [km]')
+    title('blocked edges')
+    fig = 113; % figure to plot on
+    line_spec = 'b-'; % edge line plotting
+    line_width = 2; % linewidth of the edge
+    axes_limits = [-3 3 -3 3]; % x and y axes limits
+    axis_style = 'square'; % plot axes style
+    fcn_plot_polytopes(polytopes,fig,line_spec,line_width,axes_limits,axis_style);
+    hold on
+    box on
+    xlabel('x [km]')
+    ylabel('y [km]')
+    title('all edges')
+end
+
+%% generate all_pts table
+start = [-2.5, 1];
+finish = start + [4 0];
+point_tot = length([polytopes.xv]); % total number of vertices in the convex polytopes
+beg_end = zeros(1,point_tot); % is the point the start/end of an obstacle
+curpt = 0;
+for poly = 1:size(polytopes,2) % check each polytope
+    verts = unique(polytopes(poly).vertices,'stable','rows');
+    num_verts = size(verts,1);
+    polytopes(poly).obs_id = ones(1,num_verts)*poly; % obs_id is the same for every vertex on a single polytope
+    polytopes(poly).xv = verts(:,1)';
+    polytopes(poly).yv = verts(:,2)';
+    polytopes(poly).vertices = [verts; verts(1,:)];
+    polytopes(poly).distances = fcn_general_calculation_euclidean_point_to_point_distance(polytopes(poly).vertices(1:end-1,:),polytopes(poly).vertices(2:end,:));
+    beg_end([curpt+1,curpt+num_verts]) = 1; % the first and last vertices are marked with 1 and all others are 0
+    curpt = curpt+num_verts;
+    polytopes(poly).perimeter = sum(polytopes(poly).distances);
+end
+obs_id = [polytopes.obs_id];
+point_tot = length([polytopes.xv]); % need to recheck total points
+beg_end = beg_end(1:point_tot); % remove any extra points
+
+all_pts = [[polytopes.xv];[polytopes.yv];1:point_tot;obs_id;beg_end]'; % all points [x y point_id obs_id beg_end]
+all_pts = [all_pts; start, point_tot + 1, -1, 0; finish, point_tot + 2, -1, 0];
+
+tic
+[vgraph, visibility_results] = fcn_visibility_clear_and_blocked_points_global(polytopes,all_pts,all_pts);
+toc
+% plot visibility graph edges
+if flag_do_plot
+    for i = 1:size(vgraph,1)
+        for j = 1:size(vgraph,1)
+            if vgraph(i,j) == 1
+                figure(111)
+                plot([all_pts(i,1),all_pts(j,1)],[all_pts(i,2),all_pts(j,2)],'--g','LineWidth',2)
+                figure(113)
+                plot([all_pts(i,1),all_pts(j,1)],[all_pts(i,2),all_pts(j,2)],'--g','LineWidth',2)
+            end
+            if vgraph(i,j) == 0
+                figure(112)
+                plot([all_pts(i,1),all_pts(j,1)],[all_pts(i,2),all_pts(j,2)],'--r','LineWidth',2)
+                figure(113)
+                plot([all_pts(i,1),all_pts(j,1)],[all_pts(i,2),all_pts(j,2)],'--r','LineWidth',2)
+            end
+        end
+    end
+end
+%% nonconvex polytope
+convex_polytope(1).vertices = [0 0; 1 1; 0.5, 2.5; -2, 2.5; -1 2; -2 1; -1 0; 0 0];
+polytopes = fcn_MapGen_fillPolytopeFieldsFromVertices(convex_polytope);
+
+% plot the map
+if flag_do_plot
+    fig = 121; % figure to plot on
+    line_spec = 'b-'; % edge line plotting
+    line_width = 2; % linewidth of the edge
+    axes_limits = [-3 3 -3 3]; % x and y axes limits
+    axis_style = 'square'; % plot axes style
+    fcn_plot_polytopes(polytopes,fig,line_spec,line_width,axes_limits,axis_style);
+    hold on
+    box on
+    xlabel('x [km]')
+    ylabel('y [km]')
+    title('valid edges')
+    fig = 122; % figure to plot on
+    line_spec = 'b-'; % edge line plotting
+    line_width = 2; % linewidth of the edge
+    axes_limits = [-3 3 -3 3]; % x and y axes limits
+    axis_style = 'square'; % plot axes style
+    fcn_plot_polytopes(polytopes,fig,line_spec,line_width,axes_limits,axis_style);
+    hold on
+    box on
+    xlabel('x [km]')
+    ylabel('y [km]')
+    title('blocked edges')
+    fig = 123; % figure to plot on
+    line_spec = 'b-'; % edge line plotting
+    line_width = 2; % linewidth of the edge
+    axes_limits = [-3 3 -3 3]; % x and y axes limits
+    axis_style = 'square'; % plot axes style
+    fcn_plot_polytopes(polytopes,fig,line_spec,line_width,axes_limits,axis_style);
+    hold on
+    box on
+    xlabel('x [km]')
+    ylabel('y [km]')
+    title('all edges')
+end
+
+%% generate all_pts table
+start = [-2.5, 1];
+finish = start + [4 0];
+point_tot = length([polytopes.xv]); % total number of vertices in the convex polytopes
+beg_end = zeros(1,point_tot); % is the point the start/end of an obstacle
+curpt = 0;
+for poly = 1:size(polytopes,2) % check each polytope
+    verts = unique(polytopes(poly).vertices,'stable','rows');
+    num_verts = size(verts,1);
+    polytopes(poly).obs_id = ones(1,num_verts)*poly; % obs_id is the same for every vertex on a single polytope
+    polytopes(poly).xv = verts(:,1)';
+    polytopes(poly).yv = verts(:,2)';
+    polytopes(poly).vertices = [verts; verts(1,:)];
+    polytopes(poly).distances = fcn_general_calculation_euclidean_point_to_point_distance(polytopes(poly).vertices(1:end-1,:),polytopes(poly).vertices(2:end,:));
+    beg_end([curpt+1,curpt+num_verts]) = 1; % the first and last vertices are marked with 1 and all others are 0
+    curpt = curpt+num_verts;
+    polytopes(poly).perimeter = sum(polytopes(poly).distances);
+end
+obs_id = [polytopes.obs_id];
+point_tot = length([polytopes.xv]); % need to recheck total points
+beg_end = beg_end(1:point_tot); % remove any extra points
+
+all_pts = [[polytopes.xv];[polytopes.yv];1:point_tot;obs_id;beg_end]'; % all points [x y point_id obs_id beg_end]
+all_pts = [all_pts; start, point_tot + 1, -1, 0; finish, point_tot + 2, -1, 0];
+
+tic
+[vgraph, visibility_results] = fcn_visibility_clear_and_blocked_points_global(polytopes,all_pts,all_pts);
+toc
+% plot visibility graph edges
+if flag_do_plot
+    for i = 1:size(vgraph,1)
+        for j = 1:size(vgraph,1)
+            if vgraph(i,j) == 1
+                figure(121)
+                plot([all_pts(i,1),all_pts(j,1)],[all_pts(i,2),all_pts(j,2)],'--g','LineWidth',2)
+                figure(123)
+                plot([all_pts(i,1),all_pts(j,1)],[all_pts(i,2),all_pts(j,2)],'--g','LineWidth',2)
+            end
+            if vgraph(i,j) == 0
+                figure(123)
+                plot([all_pts(i,1),all_pts(j,1)],[all_pts(i,2),all_pts(j,2)],'--r','LineWidth',2)
+                figure(122)
+                plot([all_pts(i,1),all_pts(j,1)],[all_pts(i,2),all_pts(j,2)],'--r','LineWidth',2)
+            end
+        end
+    end
+end
+return
 %% generate map
 Halton_seed = 10;
 low_pt = 1+Halton_seed; high_pt = 11+Halton_seed; % range of Halton points to use to generate the tiling
@@ -65,7 +241,7 @@ all_pts = [[polytopes.xv];[polytopes.yv];1:point_tot;obs_id;beg_end]'; % all poi
 
 %% calculate vibility graph
 tic
-[vgraph, visibility_results] = fcn_visibility_clear_and_blocked_points_global(polytopes,all_pts,gap_size);
+[vgraph, visibility_results] = fcn_visibility_clear_and_blocked_points_global(polytopes,all_pts,all_pts);
 toc
 % plot visibility graph edges
 if flag_do_plot
@@ -125,7 +301,7 @@ all_pts = [[polytopes.xv];[polytopes.yv];1:point_tot;obs_id;beg_end]'; % all poi
 
 %% calculate vibility graph
 tic
-vgraph = fcn_visibility_clear_and_blocked_points_global(polytopes,all_pts,gap_size);
+vgraph = fcn_visibility_clear_and_blocked_points_global(polytopes,all_pts,all_pts);
 toc
 deduped_pts = fcn_convert_polytope_struct_to_deduped_points(all_pts);
 % plot visibility graph edges
@@ -194,7 +370,7 @@ all_pts = [[polytopes.xv];[polytopes.yv];1:point_tot;obs_id;beg_end]'; % all poi
 
 %% calculate vibility graph
 tic
-vgraph = fcn_visibility_clear_and_blocked_points_global(polytopes,all_pts,gap_size);
+vgraph = fcn_visibility_clear_and_blocked_points_global(polytopes,all_pts,all_pts);
 toc
 deduped_pts = fcn_convert_polytope_struct_to_deduped_points(all_pts);
 % plot visibility graph edges
