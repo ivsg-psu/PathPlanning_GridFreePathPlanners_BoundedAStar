e1f12310ef8ed8b4196c2d0751f89e6d23f4214d
Stephen J. Harnett <sjh6473@arl.psu.edu>
2024-02-01 14:19:58 -0500

    first pass as corridor width matrix

---
+++ Functions/fcn_algorithm_generate_dilation_robustness_matrix.m
@@ -0,0 +1,123 @@
+function [dilation_robustness_matrix_right, dilation_robustness_matrix_left] = fcn_algorithm_generate_dilation_robustness_matrix(all_pts, start, finish, vgraph, mode)
+% fcn_algorithm_generate_dilation_robustness_matrix
+%
+% A function for generating a cost matrix and heuristic cost vector.  The cost matrix describes the
+% actual cost to go from one point to another in a map.  The heuristic vector describes the estimated
+% cost of going from each point to the goal.  If there are multiple goals, this is the minimum of
+% going to any goal because the heuristic should be an underestimate of the actual cost (see:
+% http://theory.stanford.edu/~amitp/GameProgramming/Heuristics.html).  Note using this function
+% is completely optional.  It is commonly used prior to calling a planner such as Astar
+% to conveniently generate a cost map but it may be circumvented if the user wishes to generate
+% their own cost function and cost function matrix
+%
+%
+% FORMAT:
+% dilation_robustness_matrix = fcn_algorithm_generate_dilation_robustness_matrix(all_pts, start, finish, vgraph, mode)
+%
+%
+% INPUTS:
+%
+%   start: the start point vector as (x,y,id) or (x,t,t,id)
+%
+%   finish: the finish point matrix of all valid finishes where each row is a single finish point
+%     vector as (x,y,id) or (x,y,t,id)
+%
+%   all_pts: the point matrix of all point that can be in the route, except the start and finish where
+%       each row is a single point vector (x,y,t,id)
+%
+%   vgraph: the visibility graph as an nxn matrix where n is the number of points (nodes) in the map.
+%       A 1 is in position i,j if point j is visible from point i.  0 otherwise.
+%
+%   mode: a string for what dimension the inputs are in. The mode argument must be a string with
+%     one of the following values:
+%       - "3D" - this implies xyz or xyt space
+%       - "2D" - this implies xy spatial only dimensions only
+%
+% OUTPUTS:
+%
+%  dilation_robustness_matrix - nxn matrix where n is the number of points (nodes) in the map.
+%    The value of element i,j is the estimated coridor width surrounding the line segment
+%    from point i to point j. I.e., navigating the line segment from i to j affords you Dij lateral width
+%
+% DEPENDENCIES:
+%
+% none
+%
+% EXAMPLES:
+%
+% See the script: script_fcn_algorithm_generate_dilation_robustness_matrix
+% for a full test suite.
+%
+% This function was written in January 2024 by Steve Harnett
+% Questions or comments? contact sjharnett@psu.edu
+%
+% REVISION HISTORY:
+%
+% January 2024 by Steve Harnett
+% -- first write of function
+%
+% TO DO:
+%
+% -- fill in to-do items here.
+    vgraph = vgraph - eye(size(vgraph)); % we don't want to consider self interactions here
+    all_pts = [all_pts; start; finish];
+    % initialize to zero value for each edge (zero implying the edge has no corridor width, i.e. is blocked)
+    dilation_robustness_matrix_right = zeros(size(vgraph));
+    dilation_robustness_matrix_left = zeros(size(vgraph));
+    % only need to perform this operation for 1's in vgraph, 0's are blocked edges and have 0 corridor width
+    idx_of_valid_edges = find(vgraph==1);
+    num_edges = length(idx_of_valid_edges);
+    % recall vgrpah edge ij is the line segment from point to point j
+    [edge_start_idx, edge_end_idx] = ind2sub(size(vgraph),idx_of_valid_edges);
+
+    for i = 1:num_edges
+        if strcmp(mode, "2d") || strcmp(mode,"2D")
+            edge_start = all_pts(edge_start_idx(i),1:2);
+            edge_end = all_pts(edge_end_idx(i),1:2);
+        elseif strcmp(mode, "3d") || strcmp(mode,"3D")
+            edge_start = all_pts(edge_start_idx(i),1:3);
+            edge_end = all_pts(edge_end_idx(i),1:3);
+        else
+            error(strcat("Mode argument must be a string containing '2d' or '3d' (case insensitive) mode was instead given as: ",mode))
+        end
+        % find edge direction vector
+        edge_dir = edge_end - edge_start;
+        if edge_dir(2) ~= 0
+            normal_dir(1) = 1;
+            normal_dir(2) = -(edge_dir(1)*normal_dir(1))./edge_dir(2);
+        elseif edge_dir(1) ~=0
+            normal_dir(2) = 1;
+            normal_dir(1) = -(edge_dir(2)*normal_dir(2))./edge_dir(1);
+        else
+            error("visibility graph edge has zero length")
+        end
+        normal_mag = norm(normal_dir);
+        unit_normal_right = normal_dir/normal_mag;
+        unit_normal_left = normal_dir/normal_mag;
+        % find all visibility graph edges with same origin
+        other_edge_ends_idx = find(vgraph(edge_start_idx(i), :)==1);
+        other_edge_ends_idx(find(other_edge_ends_idx==edge_end_idx(i))) = [];
+        if strcmp(mode, "2d") || strcmp(mode,"2D")
+            other_edge_ends = all_pts(other_edge_ends_idx,1:2);
+        elseif strcmp(mode, "3d") || strcmp(mode,"3D")
+            other_edge_ends = all_pts(other_edge_ends_idx,1:3);
+        else
+            error(strcat("Mode argument must be a string containing '2d' or '3d' (case insensitive) mode was instead given as: ",mode))
+        end
+        % this line removes the currently considered edge end from the other edge end list
+        other_edge_dirs = other_edge_ends - edge_start;
+        if strcmp(mode, "2d") || strcmp(mode,"2D")
+            dot_products_right = other_edge_dirs(:,1)*unit_normal_right(1) + other_edge_dirs(:,2)*unit_normal_right(2);
+            dot_products_left = other_edge_dirs(:,1)*unit_normal_left(1) + other_edge_dirs(:,2)*unit_normal_left(2);
+        elseif strcmp(mode, "3d") || strcmp(mode,"3D")
+            dot_products_right = other_edge_dirs(:,1)*unit_normal_right(1) + other_edge_dirs(:,2)*unit_normal_right(2) + other_edge_dirs(:,3)*unit_normal_right(3);
+            dot_products_left = other_edge_dirs(:,1)*unit_normal_left(1) + other_edge_dirs(:,2)*unit_normal_left(2) + other_edge_dirs(:,3)*unit_normal_left(3);
+        else
+            error(strcat("Mode argument must be a string containing '2d' or '3d' (case insensitive) mode was instead given as: ",mode))
+        end
+        corridor_width_right = min(abs(dot_products_right));
+        corridor_width_left = min(abs(dot_products_left));
+        dilation_robustness_matrix_right(edge_start_idx, edge_end_idx) = corridor_width_right;
+        dilation_robustness_matrix_left(edge_start_idx, edge_end_idx) = corridor_width_left;
+    end
+end
---
+++ Functions/script_fcn_algorithm_generate_dilation_robustness_matrix.m
@@ -0,0 +1,70 @@
+% script_fcn_algorithm_generate_dilation_robustness_matrix
+% Tests: fcn_convert_polytope_struct_to_deduped_points
+
+%
+% REVISION HISTORY:
+%
+% 2022_11_10 by S. Harnett
+% -- first write of script
+%%%%%%%%%%%%%%§
+close all; clear all; clc;
+
+addpath(strcat(pwd,'\..\..\PathPlanning_PathTools_PathClassLibrary\Functions'));
+addpath(strcat(pwd,'\..\..\PathPlanning_MapTools_MapGenClassLibrary\Functions'));
+addpath(strcat(pwd,'\..\..\Errata_Tutorials_DebugTools\Functions'));
+
+flag_do_plot = 1;
+flag_do_plot_slow = 0;
+
+fig_num = 2;
+line_width = 3;
+clear polytopes
+polytopes(1).vertices = [0 0; 4,0; 4 2; 2 4; 0 0];
+polytopes(2).vertices = [0 -1; 4, -1; 5 -2; 3 -10; 0 -1];
+polytopes = fcn_MapGen_fillPolytopeFieldsFromVertices(polytopes);
+fcn_MapGen_plotPolytopes(polytopes,fig_num,'b-',line_width);
+hold on; box on;
+
+start = ones(1,2)*(-0.5);
+finish = start;
+finish(1) = 6;
+
+plot(start(1),start(2),'xg','MarkerSize',6);
+plot(finish(1),finish(2),'xr','MarkerSize',6);
+
+point_tot = length([polytopes.xv]); % total number of vertices in the polytopes
+beg_end = zeros(1,point_tot); % is the point the start/end of an obstacle
+curpt = 0;
+for poly = 1:size(polytopes,2) % check each polytope
+    verts = length(polytopes(poly).xv);
+    polytopes(poly).obs_id = ones(1,verts)*poly; % obs_id is the same for every vertex on a single polytope
+    beg_end([curpt+1,curpt+verts]) = 1; % the first and last vertices are marked with 1 and all others are 0
+    curpt = curpt+verts;
+end
+obs_id = [polytopes.obs_id];
+all_pts = [[polytopes.xv];[polytopes.yv];1:point_tot;obs_id;beg_end]'; % all points [x y point_id obs_id beg_end]
+start = [start size(all_pts,1)+1 -1 1];
+finish = [finish size(all_pts,1)+2 -1 1];
+
+plot(all_pts(:,1), all_pts(:,2),'LineStyle','none','Marker','o','MarkerFaceColor',[255,165,0]./255);
+nudge = 1.05;
+text(all_pts(:,1)*nudge,all_pts(:,2)*nudge,string(all_pts(:,3)));
+
+finishes = [all_pts; start; finish];
+starts = [all_pts; start; finish];
+[vgraph, visibility_results_all_pts] = fcn_visibility_clear_and_blocked_points_global(polytopes, starts, finishes,1);
+% plot visibility graph edges
+if flag_do_plot_slow
+    for i = 1:size(vgraph,1)
+        for j = 1:size(vgraph,1)
+            if vgraph(i,j) == 1
+                plot([starts(i,1),starts(j,1)],[starts(i,2),starts(j,2)],'--g','LineWidth',2)
+            end
+            if vgraph(i,j) == 0
+                plot([starts(i,1),starts(j,1)],[starts(i,2),starts(j,2)],'--r','LineWidth',2)
+            end
+        end
+    end
+end
+mode = '2d';
+dilation_robustness_matrix = fcn_algorithm_generate_dilation_robustness_matrix(all_pts, start, finish, vgraph, mode)
