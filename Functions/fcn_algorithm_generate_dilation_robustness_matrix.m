f28a77ab7aff581f12a135c678bfcfef1c1df63e
Stephen J. Harnett <sjh6473@arl.psu.edu>
2024-02-14 15:30:48 -0500

    fixing variable names and comments to make method more clear

--- Functions/fcn_algorithm_generate_dilation_robustness_matrix.m
+++ Functions/fcn_algorithm_generate_dilation_robustness_matrix.m
@@ -55,6 +55,8 @@ function [dilation_robustness_matrix] = fcn_algorithm_generate_dilation_robustne
 %
 % January 2024 by Steve Harnett
 % -- first write of function
+% February 2024 by Steve Harnett
+% -- function updated to make a right left distinction using cross products
 %
 % TO DO:
 %
@@ -69,68 +71,94 @@ function [dilation_robustness_matrix] = fcn_algorithm_generate_dilation_robustne
     % recall vgrpah edge ij is the line segment from point to point j
     [edge_start_idx, edge_end_idx] = ind2sub(size(vgraph),idx_of_valid_edges);
 
+    % loop through all primary edges
     for i = 1:num_edges
+        %%  get the primary edge
+        % get the physical location of the edge start and end
         if strcmp(mode, "2d") || strcmp(mode,"2D")
-            edge_start = all_pts(edge_start_idx(i),1:2);
-            edge_end = all_pts(edge_end_idx(i),1:2);
+            primary_edge_start = all_pts(edge_start_idx(i),1:2);
+            primary_edge_end = all_pts(edge_end_idx(i),1:2);
         elseif strcmp(mode, "3d") || strcmp(mode,"3D")
-            edge_start = all_pts(edge_start_idx(i),1:3);
-            edge_end = all_pts(edge_end_idx(i),1:3);
+            primary_edge_start = all_pts(edge_start_idx(i),1:3);
+            primary_edge_end = all_pts(edge_end_idx(i),1:3);
         else
             error(strcat("Mode argument must be a string containing '2d' or '3d' (case insensitive) mode was instead given as: ",mode))
         end
         % find edge direction vector
-        edge_dir = edge_end - edge_start;
-        if edge_dir(2) ~= 0
+        primary_edge_dir = primary_edge_end - primary_edge_start;
+
+        %% get the unit normal of the primary edge
+        % calculate direction normal to edge
+        if primary_edge_dir(2) ~= 0
             normal_dir(1) = 1;
-            normal_dir(2) = -(edge_dir(1)*normal_dir(1))./edge_dir(2);
-        elseif edge_dir(1) ~=0
+            normal_dir(2) = -(primary_edge_dir(1)*normal_dir(1))./primary_edge_dir(2);
+        elseif primary_edge_dir(1) ~=0
             normal_dir(2) = 1;
-            normal_dir(1) = -(edge_dir(2)*normal_dir(2))./edge_dir(1);
+            normal_dir(1) = -(primary_edge_dir(2)*normal_dir(2))./primary_edge_dir(1);
         else
             warning("visibility graph edge has zero length")
-            corridor_width = inf;
+            corridor_width = inf; % for an edge of zero length, routing down the edge
+            % is equivalent to staying still which should have no corridor width restriction as
+            % staying still is "free" kinematically
             dilation_robustness_matrix(edge_start_idx(i), edge_end_idx(i)) = corridor_width;
-            continue
+            continue % skip to next edge if this edge had zero length
         end
+
+        normal_mag = norm(normal_dir); % find normal vector magnitude
+        unit_normal = normal_dir/normal_mag; % create unit normal from normal vector
+
+        %% get the secondary edges
+        % find all visibility graph edges with same origin (these are the secondary edges)
+        secondary_edge_ends_idx = find(vgraph(edge_start_idx(i), :)==1);
+        % remove the primary edge from the list of secondary edges
+        % i.e., we don't want to use the same edge as the primary and secondary edge because there is
+        % no corridor width (i.e., lateral spacing) between an edge and itself
+        secondary_edge_ends_idx(find(secondary_edge_ends_idx==edge_end_idx(i))) = [];
+
+        % get the physical location of the secondary edges
+        if strcmp(mode, "2d") || strcmp(mode,"2D")
+            secondary_edge_ends = all_pts(secondary_edge_ends_idx,1:2);
+        elseif strcmp(mode, "3d") || strcmp(mode,"3D")
+            secondary_edge_ends = all_pts(secondary_edge_ends_idx,1:3);
+        else
+            error(strcat("Mode argument must be a string containing '2d' or '3d' (case insensitive) mode was instead given as: ",mode))
+        end
+        % find the secondary edge direction vectors
+        secondary_edge_dirs = secondary_edge_ends - primary_edge_start;
+
+        %% discard irrelevant secondary edges
+        % we want to discard edges that either have no component in the direction of the original vector
+        % or that end at a point too far away to cut off the original vector
+
         % TODO (@sjharnett) cross edge with candidate edge - get pile of positive edges (lefts) and negatives (rights)
         % TODO set cost as sum of min of left set and min of right set
         % TODO (@sjharentt) need a tool to look up if edge is a polytope side wall
-        normal_mag = norm(normal_dir);
-        unit_normal = normal_dir/normal_mag;
-        % find all visibility graph edges with same origin
-        other_edge_ends_idx = find(vgraph(edge_start_idx(i), :)==1);
-        other_edge_ends_idx(find(other_edge_ends_idx==edge_end_idx(i))) = [];
+
+        % vector of all the dot products between the primary edge and all secondary edges
         if strcmp(mode, "2d") || strcmp(mode,"2D")
-            other_edge_ends = all_pts(other_edge_ends_idx,1:2);
+            dot_secondary_with_unit_primary = secondary_edge_dirs(:,1)*primary_edge_dir(1) + secondary_edge_dirs(:,2)*primary_edge_dir(2);
         elseif strcmp(mode, "3d") || strcmp(mode,"3D")
-            other_edge_ends = all_pts(other_edge_ends_idx,1:3);
+            dot_secondary_with_unit_primary = secondary_edge_dirs(:,1)*primary_edge_dir(1) + secondary_edge_dirs(:,2)*primary_edge_dir(2) + secondary_edge_dirs(:,3)*primary_edge_dir(3);
         else
             error(strcat("Mode argument must be a string containing '2d' or '3d' (case insensitive) mode was instead given as: ",mode))
         end
-        % this line removes the currently considered edge end from the other edge end list
-        other_edge_dirs = other_edge_ends - edge_start;
-        % we want to discard edges that either have no component in the direction of the original vector
-        % or that end at a point too far away to cut off the original vector
-        if strcmp(mode, "2d") || strcmp(mode,"2D")
-            dot_products = other_edge_dirs(:,1)*edge_dir(1) + other_edge_dirs(:,2)*edge_dir(2);
-        elseif strcmp(mode, "3d") || strcmp(mode,"3D")
-            dot_products = other_edge_dirs(:,1)*edge_dir(1) + other_edge_dirs(:,2)*edge_dir(2) + other_edge_dirs(:,3)*edge_dir(3);
-        else
-            error(strcat("Mode argument must be a string containing '2d' or '3d' (case insensitive) mode was instead given as: ",mode))
-        end
-        other_edge_projected_to_edge = dot_products/norm(edge_dir);
-        other_edge_dirs(find(other_edge_projected_to_edge <= 0 | other_edge_projected_to_edge > norm(edge_dir)),:) = [];
+        % dot the unit vector in the direction of the primary edge with the secondary edge
+        % to get the component of the secondary vector in the direction of the primary vector
+        secondary_component_in_dir_of_primary = dot_secondary_with_unit_primary/norm(primary_edge_dir);
+        % if the result is negative, the secondary edge ends "behind" the primary edge
+        % if the result is longer than the primary edge it ends "after" the primary edge
+        % both cases are discarded
+        secondary_edge_dirs(find(secondary_component_in_dir_of_primary <= 0 | secondary_component_in_dir_of_primary > norm(primary_edge_dir)),:) = [];
 
         % dot the other edges with the unit normal to find the corridor width defined by each vgraph edge
         if strcmp(mode, "2d") || strcmp(mode,"2D")
-            dot_products = other_edge_dirs(:,1)*unit_normal(1) + other_edge_dirs(:,2)*unit_normal(2);
+            dot_secondary_with_unit_normal = secondary_edge_dirs(:,1)*unit_normal(1) + secondary_edge_dirs(:,2)*unit_normal(2);
         elseif strcmp(mode, "3d") || strcmp(mode,"3D")
-            dot_products = other_edge_dirs(:,1)*unit_normal(1) + other_edge_dirs(:,2)*unit_normal(2) + other_edge_dirs(:,3)*unit_normal(3);
+            dot_secondary_with_unit_normal = secondary_edge_dirs(:,1)*unit_normal(1) + secondary_edge_dirs(:,2)*unit_normal(2) + secondary_edge_dirs(:,3)*unit_normal(3);
         else
             error(strcat("Mode argument must be a string containing '2d' or '3d' (case insensitive) mode was instead given as: ",mode))
         end
-        corridor_width = min(abs(dot_products));
+        corridor_width = min(abs(dot_secondary_with_unit_normal));
         if isempty(corridor_width)
             corridor_width = inf;
         end
