066fc70868be28c6008381b1e87558a6c3ae896e
sjharnett <sjh6473@psu.edu>
2024-04-24 09:31:35 -0400

    implement snap to vertex function

--- Functions/script_test_polytope_canyon_replan_with_dialation.m
+++ Functions/script_test_polytope_canyon_replan_with_dialation.m
@@ -252,26 +252,7 @@ for map_idx =5%2:6
             % enlarging polytopes may have put the midway start inside a polytope
             % for each polytope, check if this point is inside the polytope
             pts_to_test = [start_midway; finish_init];
-            output_pts = pts_to_test;
-            for p = 1:length(enlarged_polytopes)
-                these_verts = enlarged_polytopes(p).vertices;
-                this_polyshape = polyshape(these_verts);
-                % is point in but not on polyshape?
-                [is_in,is_on] = isinterior(this_polyshape,pts_to_test);
-                pts_in = find(is_in);
-                if isempty(pts_in)
-                    continue
-                end
-                for pt_in = 1:length(pts_in)
-                    pt_to_test = pts_to_test(pts_in(pt_in));
-                    % if it is, get distance to all vertices
-                    vert_to_start_deltas = these_verts - pt_to_test;
-                    vert_to_start_distances = vert_to_start_deltas(:,1).^2 + vert_to_start_deltas(:,2).^2;
-                    [min_value, idx_of_min] = min(vert_to_start_distances);
-                    % set this point to the nearest vertex
-                    output_pts(pts_in(pt_in),:) = these_verts(idx_of_min,:);
-                end
-            end
+            output_pts = fcn_MapGen_snapInteriorPointToVertex(enlarged_polytopes, pts_to_test);
             start_midway = output_pts(1,:);
             finish_init = output_pts(2,:);
             % TODO @sjharnett call all_pts function here again
