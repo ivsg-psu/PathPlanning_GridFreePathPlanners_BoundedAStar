a6c59e50d401542f36bd9d049aa9d73ec80a3723
sjharnett <sjh6473@psu.edu>
2024-05-09 12:43:11 -0400

    functionalize dead end removal

--- Functions/do_voronoi.m
+++ Functions/do_voronoi.m
@@ -310,19 +310,7 @@ while ~isequal(triangle_chains,prev_triangle_chains)
     end
     % TODO @sjharnett do we want to set these to zero/empty or actually remove them? Removing would require re-indexing
     %% remove dead ends
-    idx_1_connected_nodes = find(max_branching_factor == 1); % all one connected nodes are dead ends
-    % remove the node from the adjacency matrix
-    adjacency_matrix(idx_1_connected_nodes, :) = zeros(length(idx_1_connected_nodes), size(adjacency_matrix,1));
-    adjacency_matrix(:, idx_1_connected_nodes) = zeros(size(adjacency_matrix,2), length(idx_1_connected_nodes));
-    % remove the node from the node list
-    nodes(idx_1_connected_nodes) = nan;
-    % find triangle chains that start and end at this node
-    idx_chain_starts_at_1_connected_node = find(ismember([triangle_chains{:,1}]', idx_1_connected_nodes));
-    idx_chain_ends_at_1_connected_node = find(ismember([triangle_chains{:,2}]', idx_1_connected_nodes));
-    % remove these triangle chains
-    [triangle_chains{idx_chain_ends_at_1_connected_node,3}] = deal([]);
-    [triangle_chains{idx_chain_starts_at_1_connected_node,3}] = deal([]);
-
+    [adjacency_matrix, triangle_chains, nodes] = fcn_MedialAxis_removeDeadEnds(adjacency_matrix, triangle_chains, nodes, max_branching_factor);
     % plot the graph without dead ends
     figure; hold on; box on; title('medial axis graph with dead ends removed')
     xlabel('x [km]')
@@ -714,3 +702,18 @@ tit_str = sprintf('length cost weight was: %.1f \n total length: %.2f km \n wors
 title(tit_str)
 end
 end
+
+    function [adjacency_matrix, triangle_chains, nodes] = fcn_MedialAxis_removeDeadEnds(adjacency_matrix, triangle_chains, nodes, max_branching_factor)
+        idx_1_connected_nodes = find(max_branching_factor == 1); % all one connected nodes are dead ends
+        % remove the node from the adjacency matrix
+        adjacency_matrix(idx_1_connected_nodes, :) = zeros(length(idx_1_connected_nodes), size(adjacency_matrix,1));
+        adjacency_matrix(:, idx_1_connected_nodes) = zeros(size(adjacency_matrix,2), length(idx_1_connected_nodes));
+        % remove the node from the node list
+        nodes(idx_1_connected_nodes) = nan;
+        % find triangle chains that start and end at this node
+        idx_chain_starts_at_1_connected_node = find(ismember([triangle_chains{:,1}]', idx_1_connected_nodes));
+        idx_chain_ends_at_1_connected_node = find(ismember([triangle_chains{:,2}]', idx_1_connected_nodes));
+        % remove these triangle chains
+        [triangle_chains{idx_chain_ends_at_1_connected_node,3}] = deal([]);
+        [triangle_chains{idx_chain_starts_at_1_connected_node,3}] = deal([]);
+    end
