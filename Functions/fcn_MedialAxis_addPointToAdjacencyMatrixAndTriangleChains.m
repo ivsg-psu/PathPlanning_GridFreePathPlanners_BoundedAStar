040ea56b89f2e2bb3332c2e1e0cfabae8c8088b7
sjharnett <sjh6473@psu.edu>
2024-06-25 15:50:43 -0400

    comments

--- Functions/fcn_MedialAxis_addPointToAdjacencyMatrixAndTriangleChains.m
+++ Functions/fcn_MedialAxis_addPointToAdjacencyMatrixAndTriangleChains.m
@@ -1,14 +1,107 @@
 function [adjacency_matrix, triangle_chains, nodes, point_closest_tri, point_closest_node] = fcn_MedialAxis_addPointToAdjacencyMatrixAndTriangleChains(point_xy, adjacency_matrix, triangle_chains, nodes, xcc, ycc, max_side_lengths_per_tri)
-    % pick a start and finish
-    % start = all_pts(24,:);
-    % finish = all_pts(104,:);
-    % find the xcc,ycc pair closest to start
-    % start_xy = [1031.5 -4715.4];
+% fcn_MedialAxis_addPointToAdjacencyMatrixAndTriangleChains
+%
+% This function is useful for adding arbitrary points (such as a start or finish)
+% to the medial axis graph (composed of an adjacency matrix and the
+% triangle_chains structure describing the edges).  The function places a node at
+% the nearest point on the medial axis to the arbitrary point.  I.e., the closest
+% edge to the new point is identified and a node is placed along that edge such
+% that it is closest along the existing edge to the new point.
+% This node can then be attached to the arbitrary point via a straight line which
+% extends normally from the medial axes.  This creates a scenario where, if the
+% vehicle starts at the new point, it can drive straight to the new node
+% in the shortest distance possible to center in the corridor and route along
+% medial axes.
+%
+%
+% FORMAT:
+%
+% [adjacency_matrix, triangle_chains, nodes, point_closest_tri, point_closest_node] = fcn_MedialAxis_addPointToAdjacencyMatrixAndTriangleChains(point_xy, adjacency_matrix, triangle_chains, nodes, xcc, ycc, max_side_lengths_per_tri)
+%
+%
+% INPUTS:
+%
+%    Useful variables for outputs: N - number of nodes, M - number of edges, P_M - number of triangles
+%       in the Mth edge, Q - number of triangles in the triangulation.
+%
+%    point_xy: 1x2 double of the xy coordinates of the point to add to the medial axis graph
+%
+%    adjacency_matrix: Like the visibility graph but rather than indicating 2 nodes are visible,
+%       it indicates 2 nodes are connected by an edge.
+%       This an NxN matrix where N is the number of nodes in the map.
+%       A 1 is in position i,j if node j is visible from point i.  0 otherwise.
+%
+%    triangle_chains: an Mx5 cell array with a row for each edge in the medial axis graph.  The first
+%      column contains an int for the node ID for the start of the chain.  The second is the end node.
+%      The third column is a 1xP_M array of integers representing IDs of the triangles whose circumcenters
+%      form the "chain of triangles" connecting the two nodes. P_M can be different for each row, M.
+%      The 4th column contains the estimated corridor width (the minimum lateral free space a vehicle
+%      would have when routing down the edge) and the 5th column contains the length of the edge.
+%
+%    nodes: a Nx1 array of integers.  The integers are the IDs of the triangles that are 3-connected,
+%      i.e., their circumcenters are nodes in the medial axis graph.  The position in the nodes array
+%      is the node ID and the value is the triangle ID.  E.g., if nodes(10)=146, then the 10th node
+%      in the adjacency_matrix and triangle_chains struct is the 146th triangle in
+%      the Delaunay triangulation.
+%
+%    xcc: Qx1 array of doubles.  The x positions of the circumcenters of the triangles.
+%
+%    ycc: Qx1 array of doubles.  The y positions of the circumcenters of the triangles.
+%
+%    max_side_lengths_per_tri: a Qx1 array of doubles.  The double is the length of the shortest
+%      side of each triangle.
+%
+%
+% OUTPUTS:
+%    Useful variables for outputs: N - number of nodes, M - number of edges, P_M - number of triangles
+%       in the Mth edge, Q - number of triangles in the triangulation.
+%
+%    adjacency_matrix: same as the input but with an additional row and column for the added node
+%
+%    triangle_chains: same as the input but with 4 additional tri chains to connect the added node
+%      which will be midway along an existing edge, the original 2 ends of that edge. I.e., if the
+%      nearest edge to new point k is edge i-j, tri chains will be added for:
+%        - i-k
+%        - k-j
+%        - k-i
+%        - j-k
+%
+%    nodes: same as the input but with an additional triangle added as a node, selected such that
+%      it is added along an existing edge at the location on the
+%      medial axis graph closest to the arbitrary point.
+%
+%    point_closest_tri: the ID of the triangle whose circumcenter is closest to the input point
+%
+%    point_closest_node: the ID of the new node added to be is closest to the input point
+%
+%
+% DEPENDENCIES:
+%
+%
+% EXAMPLES:
+%
+% See the script: script_test_voronoi_planning* for examples of the script in use.
+% See ../Documentation/medial_axis_planning.pptx for a flow chart of the medial axis/voronoi planning stack
+%
+% This function was written Spring 2024 by Steve Harnett
+% Questions or comments? contact sjharnett@psu.edu
+
+%
+% REVISION HISTORY:
+%
+% 2024, Spring by Steve Harnett
+% -- first write of function
+%
+% TO DO:
+%
+% -- fill in to-do items here.
+    % don't want to look at all triangles in constrained triangulation, TR as not all of them are medial axis edges
     tris_in_graph = unique([triangle_chains{:,3}]');
+    % find distance from arbitrary point to circumcenters of all triangles in graph
     point_delta_from_all_tris = point_xy - [xcc(tris_in_graph), ycc(tris_in_graph)];
     point_dist_from_all_tris = (point_delta_from_all_tris(:,1).^2 + point_delta_from_all_tris(:,2).^2).^0.5;
-    [~, point_closest_tri_loc] = min(point_dist_from_all_tris);
-    point_closest_tri = tris_in_graph(point_closest_tri_loc);
+    [~, point_closest_tri_loc] = min(point_dist_from_all_tris); % find closest circumcenter
+    point_closest_tri = tris_in_graph(point_closest_tri_loc); % note the closest triangle circumcenter
     % find which chains the closest tri is in (should really only be 2)
     idx_chains_containing_point = [];
     for i = 1:(size(triangle_chains,1))
@@ -36,7 +129,7 @@ function [adjacency_matrix, triangle_chains, nodes, point_closest_tri, point_clo
         triangle_chains{end+1,1} = first_node;
         triangle_chains{end,2} = point_closest_node;
         triangle_chains{end,3} = first_chain;
-        triangle_chains{end,4} = min(max_side_lengths_per_tri(first_chain));
+        triangle_chains{end,4} = min(max_side_lengths_per_tri(first_chain)); % add min corridor width
         % the cumulative length of all the distance between circumcenters is the triangle chain length
         delta_x_and_y = diff([xcc(first_chain) ycc(first_chain)]);
         triangle_chain_length = sum(sqrt(sum(delta_x_and_y.*delta_x_and_y,2)));
@@ -44,7 +137,7 @@ function [adjacency_matrix, triangle_chains, nodes, point_closest_tri, point_clo
         triangle_chains{end+1,1} = point_closest_node;
         triangle_chains{end,2} = last_node;
         triangle_chains{end,3} = last_chain;
-        triangle_chains{end,4} = min(max_side_lengths_per_tri(last_chain));
+        triangle_chains{end,4} = min(max_side_lengths_per_tri(last_chain)); % add min corridor width
         % the cumulative length of all the distance between circumcenters is the triangle chain length
         delta_x_and_y = diff([xcc(last_chain) ycc(last_chain)]);
         triangle_chain_length = sum(sqrt(sum(delta_x_and_y.*delta_x_and_y,2)));
--- Functions/fcn_MedialAxis_makeCostGraphAndAllPoints.m
+++ Functions/fcn_MedialAxis_makeCostGraphAndAllPoints.m
@@ -12,62 +12,104 @@ function [adjacency_matrix, cgraph, all_pts, start, finish, best_chain_idx_matri
 %
 % INPUTS:
 %
-%   start: the start point vector (x,y,id)
+%    adjacency_matrix: Like the visibility graph but rather than indicating 2 nodes are visible,
+%       it indicates 2 nodes are connected by an edge.
+%       This an NxN matrix where N is the number of nodes in the map.
+%       A 1 is in position i,j if node j is visible from point i.  0 otherwise.
 %
-%   finish: the finish point matrix of all valid finishes where each row is a single finish point vector (x,y,id)
+%    triangle_chains: an Mx5 cell array with a row for each edge in the medial axis graph.  The first
+%      column contains an int for the node ID for the start of the chain.  The second is the end node.
+%      The third column is a 1xP_M array of integers representing IDs of the triangles whose circumcenters
+%      form the "chain of triangles" connecting the two nodes. P_M can be different for each row, M.
+%      The 4th column contains the estimated corridor width (the minimum lateral free space a vehicle
+%      would have when routing down the edge) and the 5th column contains the length of the edge.
 %
-%   all_pts: the point matrix of all point that can be in the route, except the start and finish where
-%       each row is a single point vector (x,y,id)
+%    nodes: a Nx1 array of integers.  The integers are the IDs of the triangles that are 3-connected,
+%      i.e., their circumcenters are nodes in the medial axis graph.  The position in the nodes array
+%      is the node ID and the value is the triangle ID.  E.g., if nodes(10)=146, then the 10th node
+%      in the adjacency_matrix and triangle_chains struct is the 146th triangle in
+%      the Delaunay triangulation.
 %
-%   vgraph: the visibility graph as an nxn matrix where n is the number of points (nodes) in the map.
-%       A 1 is in position i,j if point j is visible from point i.  0 otherwise.
+%    xcc: Qx1 array of doubles.  The x positions of the circumcenters of the triangles.
+%
+%    ycc: Qx1 array of doubles.  The y positions of the circumcenters of the triangles.
+%
+%    start_closest_tri: the ID of the triangle whose circumcenter is closest to the start point
+%
+%    start_closest_node: the ID of the new node added to be is closest to the start point
+%
+%    finish_closest_tri: the ID of the triangle whose circumcenter is closest to the finish point
+%
+%    finish_closest_node: the ID of the new node added to be is closest to the finish point
+%
+%    w: scalar relative weighting of cost function, cost = w*length_cost + (1-w)*corridor width
+%
+%    min_corridor_width: scalar value of the minimum allowable corridor width.  Edges (triangle chains)
+%      that have a smaller estimated corridor width will be filtered out
+%
+%    denylist_route_chain_ids: array of integer values of rows in triangle_chains that should not be
+%       routeable.  This is useful for filtering out edges from the medial axis graph that you
+%       do not wish to consider in path planning for arbitrary reasons such as not using edges
+%       that have been previously used, manually marking edges block to "close a road" so to speak,
+%       etc.of the minimum allowable corridor width.  Edges (triangle chains)
+%
+%
+% OUTPUTS:
+%
+%    adjacency_matrix: Like the visibility graph but rather than indicating 2 nodes are visible,
+%       it indicates 2 nodes are connected by an edge.
+%       This an NxN matrix where N is the number of nodes in the map.
+%       A 1 is in position i,j if node j is visible from point i.  0 otherwise.
 %
 %    cgraph: the cost graph matrix. A cost matrix is an nxn matrix where n is
 %      the number of points (nodes) in the map including the start and goal.
 %      The value of element i-j is the cost of routing from i to j.
 %
-%    hvec: the heuristic cost vector. A 1xn vector where n is
-%      the number of points (nodes) in the map including the start and goal.
-%      The value of element k is the estimated cost of routing from point k to
-%      the finish based on a heuristic cost estimation method.
+%   all_pts: the point matrix of all nodes that the planner can consider, except the start and finish where
+%       each row is a single point vector (x,y,id)
 %
-% OUTPUTS:
+%   start: the start point vector (x,y,id)
 %
-%     cost: the total cost of the selected route
+%   finish: the finish point matrix of all valid finishes where each row is a single finish point vector (x,y,id)
 %
-%    route: the matrix as produced by fcn_algorithm_Astar3d consisting of waypoints.  Each row is a
-%    waypoint, and each column is x, y, and point ID
+%    best_chain_idx_matrix: Like the visibility graph but rather than indicating 2 nodes are visible,
+%       it indicates the best (lowest cost) triangle chain (edge) connecting 2 nodes.
+%       This an NxN matrix where N is the number of nodes in the map.
+%       A 12 is in position i,j if the best triangle chain to the node in nodes(j) from the node in
+%       nodes(i) is located in triangle_chains{12,:}. The reason this matrix exists is because there
+%       may be multiple triangle chains connecting the same two nodes in the medial axis graph (unlike
+%       in the visibility graph) so the planner needs to evaluate the cost from i to j by looking
+%       at the optimal cost available.
+%
 %
 %
 % DEPENDENCIES:
 %
-% none but several functions exist to create visibility matrices and fcn_algorithm_generate_cost_graph can create cost matrices (cgraph) and heuristic cost vectors (hvec)
 %
 % EXAMPLES:
 %
-% See the script: script_test_fcn_algorithm_Astar
-% for a full test suite.
+% See the script: script_test_voronoi_planning* for examples of the script in use.
+% See ../Documentation/medial_axis_planning.pptx for a flow chart of the medial axis/voronoi planning stack
 %
-% This function was written on spring 2023 by Steve Harnett
+% This function was written Spring 2024 by Steve Harnett
 % Questions or comments? contact sjharnett@psu.edu
 
 %
 % REVISION HISTORY:
 %
-% 2023, spring by Steve Harnett
+% 2024, Spring by Steve Harnett
 % -- first write of function
 %
 % TO DO:
 %
 % -- fill in to-do items here.
-
     num_nodes = length(nodes);
     all_pts = nan(num_nodes,3);
     for i = 1:num_nodes
         if isnan(nodes(i))
             continue
         end
-        all_pts(i,:) = [xcc(nodes(i)), ycc(nodes(i)), i];
+        all_pts(i,:) = [xcc(nodes(i)), ycc(nodes(i)), i]; % use the circumcenter of triangle node(i) as the position
     end
     %% form cost graph from triangle_chains
     % cost is of the form: total cost = w*length + (1-w)*corridor_width
@@ -85,7 +127,8 @@ function [adjacency_matrix, cgraph, all_pts, start, finish, best_chain_idx_matri
         % find all the chains connecting r and c in adjacency that also meet minimum corridor width requirement
         idx_chain_rc = find([triangle_chains{:,1}]'== r(i) & [triangle_chains{:,2}]'== c(i) & [triangle_chains{:,4}]' > min_corridor_width);
         % also need to allow for filtering on banned chains
-        idx_chain_rc = setdiff(idx_chain_rc, denylist_route_chain_ids); % want to not use triangle chains that were in previous routes
+        % in case we want to not use triangle chains that were in previous routes or we noticed a route was blocked and want to manually denylist it
+        idx_chain_rc = setdiff(idx_chain_rc, denylist_route_chain_ids);
         % if there are no matches meeting the start, goal, and min corridor width, set adjacency to zero and move on
         if isempty(idx_chain_rc)
             adjacency_matrix(r(i),c(i)) = 0;
@@ -94,12 +137,11 @@ function [adjacency_matrix, cgraph, all_pts, start, finish, best_chain_idx_matri
         % we want to only use the chain with the lowest total cost form r to c
         corridor_widths = [triangle_chains{idx_chain_rc, 4}]; % the corridor width of all valid chains
         lengths = [triangle_chains{idx_chain_rc, 5}]; % the length of all valid chains
-        possible_costs = w*lengths + (1-w)*(corridor_widths).^(-1); % vectorized total cost
+        possible_costs = w*lengths + (1-w)*(corridor_widths).^(-1); % vectorized total cost (note we use inverse of width because width is good and length is bad)
         [min_cost, min_cost_location] = min(possible_costs); % the min cost is what we use as cost
         cgraph(r(i),c(i)) = min_cost;
         best_chain_idx_matrix(r(i),c(i)) = idx_chain_rc(min_cost_location); % need to remember which chain we want to use
     end
-
     %  set the start for the planner as the start node not the startxy
     start = [xcc(start_closest_tri) ycc(start_closest_tri) start_closest_node];
     finish = [xcc(finish_closest_tri) ycc(finish_closest_tri) finish_closest_node];
