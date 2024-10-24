910da154b269e983c8267b16f6f4ce6df144bd80
sjharnett <sjh6473@psu.edu>
2024-10-24 19:59:01 -0400

    repeat graph creation for timing data

--- Functions/script_test_voronoi_planning.m
+++ Functions/script_test_voronoi_planning.m
@@ -7,12 +7,14 @@ addpath(strcat(pwd,'\..\..\PathPlanning_MapTools_MapGenClassLibrary\Functions'))
 addpath(strcat(pwd,'\..\..\Errata_Tutorials_DebugTools\Functions'));
 
 
-map_idx = 7;
+map_idx = 6;
 flag_do_plot = 1;
 flag_do_animation = 0;
 flag_do_plot_slow = 0;
+num_time_trials = 1; % repeat iterations of map creation to get timing data
 [shrunk_polytopes, start_init, finish_init, resolution_scale] = fcn_util_load_test_map(map_idx, 1);
-
+start_init = start_init(2,:);
+finish_init = finish_init(2,:);
 %% for sake of comparisons, do this with a visibility graph as well
 polytopes = shrunk_polytopes;
 if flag_do_plot
@@ -47,7 +49,7 @@ point_tot = length([polytopes.xv]); % need to recheck total points
 beg_end = beg_end(1:point_tot); % remove any extra points
 all_pts = [[polytopes.xv];[polytopes.yv];1:point_tot;obs_id;beg_end]'; % all points [x y point_id obs_id beg_end]
 vgraph_times = [];
-for i = 1:10 % get timing data for repeat iterations
+for i = 1:num_time_trials % get timing data for repeat iterations
     create_vgraph_timer = tic;
     [vgraph, visibility_results] = fcn_visibility_clear_and_blocked_points_global(polytopes,all_pts,all_pts);
     vgraph_time = toc(create_vgraph_timer)
@@ -64,7 +66,7 @@ if flag_do_plot
     end
 end
 med_ax_times = [];
-for i = 1:10 % get timing data for repeat iterations
+for i = 1:num_time_trials % get timing data for repeat iterations
     t_medial_axis = tic;
     %% constrained delaunay triangulation
     [adjacency_matrix, triangle_chains, nodes, xcc, ycc, tr] = fcn_MedialAxis_makeAdjacencyMatrixAndTriangleChains(shrunk_polytopes, resolution_scale, flag_do_plot);
@@ -88,7 +90,7 @@ finish_xy = finish_init;
 [adjacency_matrix, triangle_chains, nodes, finish_closest_tri, finish_closest_node] = fcn_MedialAxis_addPointToAdjacencyMatrixAndTriangleChains(finish_xy, adjacency_matrix, triangle_chains, nodes, xcc, ycc, max_side_lengths_per_tri);
 
 % loop over cost function weights
-for w = 0.1:0.1:1
+for w = 0:0.1:1
     min_corridor_width = 0; % do not restrict corridor width
     denylist_route_chain_ids = []; % no need to denylist any triangle chains
     % make cost matrix
@@ -156,6 +158,7 @@ for w = 0.1:0.1:1
     plot(route_full(:,1), route_full(:,2), '-k','LineWidth',2.5) % plot approx. medial axis
     leg_str{end+1} = 'medial axis route';
     legend(leg_str,'Location','best');
-    tit_str = sprintf('length cost weight was: %.1f \n total length: %.2f km \n worst corridor: %.2f km',w, route_length, route_choke);
-    title(tit_str)
+    tit_str = sprintf('%.1f $\n total length: $%.2f~km$ \n worst corridor width: $%.2f~km$',w, route_length, route_choke);
+    tit_str = strcat('$\alpha: ', tit_str)
+    title(tit_str,'Interpreter','latex')
 end % end weight loop
