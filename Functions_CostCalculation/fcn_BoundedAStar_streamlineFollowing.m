3f0005fa1c89c3098db6fd828500f28f977e17a2
kaeleahayes <kaeleahayes@gmail.com>
2025-07-30 15:37:59 -0400

    FEAT: added function to demonstrate streamline following

---
+++ Functions_CostCalculation/fcn_BoundedAStar_streamlineFollowing.m
@@ -0,0 +1,215 @@
+function trajectory = fcn_BoundedAStar_streamlineFollowing(startPoint, timeLength, x, y, windFieldU, windFieldV, varargin)
+% fcn_BoundedAStar_sampleWindField
+% samples a wind field near a given point and outputs the resulting wind
+% vector
+%
+% FORMAT:
+% [windVector] = fcn_BoundedAStar_sampleWindField(samplePoint, x, y, windFieldU, windFieldV, (fig_num))
+%
+% INPUTS:
+%
+%     startPoint: the [x, y] point to drop a particle into the wind field 
+%
+%     timeLength: the length of time to propagate the simulation for
+%
+%     x: a 1xn vector containing the x values assigned to each wind field
+%     grid point 
+%
+%     y: a 1xn vector containing the y values assigned to each wind field
+%     grid point
+%
+%     windFieldU: a nxn matrix containing the u-direction components of the
+%     wind velocity at each grid point
+%
+%     windFieldV: a nxn matrix containing the v-direction components of the
+%     wind velocity at each grid point
+%
+%     (optional inputs)
+%
+%     fig_num: a figure number to plot results. If set to -1, skips any
+%     input checking or debugging, no figures will be generated, and sets
+%     up code to maximize speed. As well, if given, this forces the
+%     variable types to be displayed as output and as well makes the input
+%     check process verbose.
+%
+% OUTPUTS:
+%
+%     trajectory: the stored trajectory of the point over time
+%
+% DEPENDENCIES:
+%
+%     fcn_DebugTools_checkInputsToFunctions
+%
+% EXAMPLES:
+%
+% See the script: script_test_fcn_BoundedAStar_streamlineFollowing
+% for a full test suite.
+%
+% This function was written on 2025_07_30 by K. Hayes
+% Questions or comments? contact kxh1031@psu.edu
+
+% REVISION HISTORY:
+% 2025_07_30 by K. Hayes
+% -- first write of function using fcn_BoundedAStar_sampleWindField as a
+%    starter
+
+% TO-DO
+% -- (none)
+
+%% Debugging and Input checks
+% Check if flag_max_speed set. This occurs if the fig_num variable input
+% argument (varargin) is given a number of -1, which is not a valid figure
+% number.
+MAX_NARGIN =7; % The largest Number of argument inputs to the function
+flag_max_speed = 0;
+if (nargin==MAX_NARGIN && isequal(varargin{end},-1))
+    flag_do_debug = 0; %     % Flag to plot the results for debugging
+    flag_check_inputs = 0; % Flag to perform input checking
+    flag_max_speed = 1;
+else
+    % Check to see if we are externally setting debug mode to be "on"
+    flag_do_debug = 0; %     % Flag to plot the results for debugging
+    flag_check_inputs = 1; % Flag to perform input checking
+    MATLABFLAG_MAPGEN_FLAG_CHECK_INPUTS = getenv("MATLABFLAG_MAPGEN_FLAG_CHECK_INPUTS");
+    MATLABFLAG_MAPGEN_FLAG_DO_DEBUG = getenv("MATLABFLAG_MAPGEN_FLAG_DO_DEBUG");
+    if ~isempty(MATLABFLAG_MAPGEN_FLAG_CHECK_INPUTS) && ~isempty(MATLABFLAG_MAPGEN_FLAG_DO_DEBUG)
+        flag_do_debug = str2double(MATLABFLAG_MAPGEN_FLAG_DO_DEBUG);
+        flag_check_inputs  = str2double(MATLABFLAG_MAPGEN_FLAG_CHECK_INPUTS);
+    end
+end
+
+% flag_do_debug = 1;
+
+if flag_do_debug
+    st = dbstack; %#ok<*UNRCH>
+    fprintf(1,'STARTING function: %s, in file: %s\n',st(1).name,st(1).file);
+    debug_fig_num = 999978; %#ok<NASGU>
+else
+    debug_fig_num = []; %#ok<NASGU>
+end
+
+%% check input arguments?
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   _____                   _
+%  |_   _|                 | |
+%    | |  _ __  _ __  _   _| |_ ___
+%    | | | '_ \| '_ \| | | | __/ __|
+%   _| |_| | | | |_) | |_| | |_\__ \
+%  |_____|_| |_| .__/ \__,_|\__|___/
+%              | |
+%              |_|
+% See: http://patorjk.com/software/taag/#p=display&f=Big&t=Inputs
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if 0==flag_max_speed
+    if flag_check_inputs
+        % Are there the right number of inputs?
+        narginchk(0,MAX_NARGIN);
+        
+        %%%%% No required inputs, delete these?
+        % % Check the XY_range input, make sure it is '4column_of_numbers'
+        % % type with exactly 1 row
+        % fcn_DebugTools_checkInputsToFunctions(...
+        %     XY_range, '4column_of_numbers',[1 1]);
+        % 
+        % % Check the radius input, make sure it is '1column_of_numbers'
+        % % type, 1 row
+        % fcn_DebugTools_checkInputsToFunctions(...
+        %     radius, '1column_of_numbers',[1 1]);
+    end
+end
+
+% Does user want to show the plots?
+flag_do_plots = 0; % Default is to NOT show plots
+if (0==flag_max_speed) && (MAX_NARGIN == nargin) 
+    temp = varargin{end};
+    if ~isempty(temp) % Did the user NOT give an empty figure number?
+        fig_num = temp;
+        figure(fig_num);
+        flag_do_plots = 1;
+    end
+end
+
+%% Main code
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   __  __       _
+%  |  \/  |     (_)
+%  | \  / | __ _ _ _ __
+%  | |\/| |/ _` | | '_ \
+%  | |  | | (_| | | | | |
+%  |_|  |_|\__,_|_|_| |_|
+%
+%See: http://patorjk.com/software/taag/#p=display&f=Big&t=Main
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%§
+
+% Create state matrices
+deltat = 0.01;
+numSteps = timeLength/deltat;
+trajectory = nan*ones(numSteps,2);
+trajectory(1,:) = startPoint;
+currentPoint = startPoint;
+
+for k = 1:numSteps
+    % Find wind vector at point and calculate disturbance
+    windVector = fcn_BoundedAStar_sampleWindField(currentPoint, x, y, windFieldU, windFieldV, -1);
+    disturbance = windVector/numSteps;
+    
+    % Add disturbance to postion
+    trajectory(k+1,:) = currentPoint + disturbance;
+    
+    % Update position
+    currentPoint = trajectory(k+1,:);
+end
+
+
+%% Plot the results (for debugging)?
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   _____       _
+%  |  __ \     | |
+%  | |  | | ___| |__  _   _  __ _
+%  | |  | |/ _ \ '_ \| | | |/ _` |
+%  | |__| |  __/ |_) | |_| | (_| |
+%  |_____/ \___|_.__/ \__,_|\__, |
+%                            __/ |
+%                           |___/
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+if flag_do_plots
+    % Plot setup
+    figure(fig_num)
+    hold on
+    grid on
+    axis equal
+
+    % Plot wind field
+    fcn_BoundedAStar_plotWindField(windFieldU, windFieldV, x, y, 'default',(fig_num))
+    
+    % Plot trajectory
+    plot(trajectory(:,1), trajectory(:,2), 'k.', 'MarkerSize', 20, 'DisplayName', 'Trajectory')
+    
+    % Display legend
+    legend
+
+end % Ends the flag_do_plot if statement
+
+if flag_do_debug
+    fprintf(1,'ENDING function: %s, in file: %s\n\n',st(1).name,st(1).file);
+end
+
+
+end % Ends the main function
+
+
+
+%% Functions follow
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   ______                _   _
+%  |  ____|              | | (_)
+%  | |__ _   _ _ __   ___| |_ _  ___  _ __  ___
+%  |  __| | | | '_ \ / __| __| |/ _ \| '_ \/ __|
+%  | |  | |_| | | | | (__| |_| | (_) | | | \__ \
+%  |_|   \__,_|_| |_|\___|\__|_|\___/|_| |_|___/
+%
+% See: https://patorjk.com/software/taag/#p=display&f=Big&t=Functions
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%§
+
--- Functions_CostCalculation/script_test_fcn_BoundedAStar_streamlineFollowing.m
+++ Functions_CostCalculation/script_test_fcn_BoundedAStar_streamlineFollowing.m
@@ -1,17 +1,15 @@
-% script_test_fcn_BoundedAStar_matrixEnvelopeExpansion
-% Tests: fcn_BoundedAStar_matrixEnvelopeExpansion
+% script_test_fcn_BoundedAStar_streamlineFollowing
+% Tests: fcn_BoundedAStar_streamlineFollowing
 
 % Revision history
-% 2025_07_23 by K. Hayes, kxh1031@psu.edu
+% 2025_07_30 by K. Hayes, kxh1031@psu.edu
 % -- first write of script
-% 2025_07_24 by K. Hayes
-% -- fixed formatting of script
-% 2025_07_29 by K. Hayes
-% -- added test case for debugging of streamline-following behavior
 
 % TO DO:
 % -- take wind post-processing out of this script and put it into another
 %    separate function
+% -- fix bug where propagating the time for longer doesn't change distance
+%    traveled
 
 
 %% Set up the workspace
@@ -35,9 +33,9 @@ close all
 close all;
 fprintf(1,'Figure: 1XXXXXX: DEMO cases\n');
 
-%% DEMO case: get reachable envelope in wind field
+%% DEMO case: drop a point into a wind field and follow a streamline
 fig_num = 10001;
-titleString = sprintf('DEMO case: get reachable envelope in wind field');
+titleString = sprintf('DEMO case: drop a point into a wind field and follow a streamline');
 fprintf(1,'Figure %.0f: %s\n',fig_num, titleString);
 figure(fig_num); clf;
 
@@ -101,27 +99,20 @@ normalizedNorthWind = 10*northWind./maxWind;
 
 %%%%
 
-% Call graph generation function
-x = linspace(XY_range(1), XY_range(3), nRows);
-y = linspace(XY_range(2), XY_range(4), mColumns);
-start = [0.2, 0.9 , n_nodes+1, -1, 0];
-finish = [0.6, 0.1, n_nodes+2, -1, 0];
-
-% Call set expansion methods
-radius = 5;
-centerPoint = [0 0];
-expandedSets = fcn_BoundedAStar_matrixEnvelopeExpansion(radius,normalizedEastWind,normalizedNorthWind,x,y, (centerPoint), (fig_num));
+% Put a point into the wind field and show the trajectory along a
+% streamline
+startPoint = [4 -6];
+timeLength = 5;
+trajectory = fcn_BoundedAStar_streamlineFollowing(startPoint, timeLength, x, y, normalizedEastWind, normalizedNorthWind, (fig_num));
 
 sgtitle(titleString, 'Interpreter','none');
 
 % Check variable types
-assert(isnumeric(expandedSets));
+assert(isnumeric(trajectory));
 
 % Check variable sizes
-Npoints = 629;
-assert(size(expandedSets,1)==Npoints); 
-assert(size(expandedSets,2)==2);
-assert(size(expandedSets,3)==11);
+assert(size(trajectory,1)==2001); 
+assert(size(trajectory,2)==2);
 
 % Make sure plot opened up
 assert(isequal(get(gcf,'Number'),fig_num));
@@ -151,89 +142,7 @@ titleString = sprintf('TEST case: Drop a point on a streamline');
 fprintf(1,'Figure %.0f: %s\n',fig_num, titleString);
 figure(fig_num); clf;
 
-% Fill inputs
-randomSeed = 4822262;
-windMagnitude = [];
-NpointsInSide = [];
-XY_range = [-10 -10 10 10];
-peaksMode = [];
-n_nodes = 10;
-rngSeed = [];
 
-% Call random occupancy map function - code taken from
-% script_demo_generateRandomOccupancyAnimated
-rng(2020)
-nRows = 50;
-mColumns = 50;
-mapSize = [nRows mColumns];
-
-Nsteps = 50;
-Ncontours = 30;
-movementSideways = 1; % 0.6; %.5; %2.3;
-
-occupancyRatio = 0.2;
-dilationLevel = 400;
-seedMap = rand(nRows,mColumns);
-initialSeedMap = seedMap;
-Nseeds = numel(seedMap);
-leftDilationMultiplier = [];
-rightDilationMultiplier = [];
-optimizedThreshold = [];
-flag_firstDraw = 1;
-
-% Call the function once to initialize settings for upcoming calls
-[occupancyMatrix, randomMatrixDilated, forcedThreshold, leftDilationMultiplier, rightDilationMultiplier] = ...
-    fcn_GridMapGen_generateRandomOccupancyMap(...
-    'mapSize', (mapSize),... % [nRows mCols])
-    'occupancyRatio',(occupancyRatio),... % [1x1] value between 0 and 1
-    'dilationLevel',(dilationLevel),.... % [1x1] strictly positive int
-    'seedMap', (seedMap),... % [1x1] integer to be a random seed or NxM matrix of random numbers
-    'leftDilationMultiplier', (leftDilationMultiplier),... %  [nRows nRows], ...
-    'rightDilationMultiplier', (rightDilationMultiplier),... % [mCols mCols], ...
-    'thresholdForced', (optimizedThreshold), ... % [1x1] scalar
-    'flagSkipThresholdOptimization',(0),...% [1x1] scalar
-    'figNum',(-1));
-
-
-%%%% WIND POST-PROCESSING SHOULD BE MADE INTO AN ALTERNATE FUNCTION (?). FOR
-%%%% NOW THE CODE IS JUST COPIED.
-% Use the gradient to estimate wind direction
-[px,py] = gradient(randomMatrixDilated);
-eastWind  = py;
-northWind = -px;
-
-% Solve for the wind magnitude
-windMagnitude = (eastWind.^2+northWind.^2).^0.5;
-maxWind = max(windMagnitude,[],'all');
-normalizedWindMagnitude = windMagnitude./maxWind;
-normalizedEastWind = 10*eastWind./maxWind;
-normalizedNorthWind = 10*northWind./maxWind;
-
-%%%%
-
-% Call graph generation function
-x = linspace(XY_range(1), XY_range(3), nRows);
-y = linspace(XY_range(2), XY_range(4), mColumns);
-start = [0.2, 0.9 , n_nodes+1, -1, 0];
-finish = [0.6, 0.1, n_nodes+2, -1, 0];
-
-% Call set expansion methods
-radius = 5;
-centerPoint = [2 8];
-[expandedSets] = fcn_BoundedAStar_matrixEnvelopeExpansion(radius,normalizedEastWind,normalizedNorthWind,x,y, (centerPoint), (-1));
-
-sgtitle(titleString, 'Interpreter','none');
-
-% Create figure
-figure(fig_num)
-hold on
-axis equal
-s = streamslice(x,y,normalizedEastWind,normalizedNorthWind);
-set(s,'Color',[0.6 0.6 0.6])
-for i = 1:size(expandedSets,3)
-    plot(expandedSets(1,1,i),expandedSets(1,2,i),'b.','MarkerSize', 10)
-    drawnow
-end
 
 
 %% Fast Mode Tests
